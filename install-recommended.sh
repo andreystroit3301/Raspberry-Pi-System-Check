f9dfcf0ebc46cd3d17a9c1803016824384127b71
Andrey Stroitelev <andrey.stroitelev3301@gmail.com>
2026-01-03 18:05:31 -0600

    Update install-recommended.sh to install sysload
    added short section to pull sysload from github and install it. This utility is used for getting the CPU and GPU usage for the system-status script.

--- install-recommended.sh
+++ install-recommended.sh
@@ -4,7 +4,17 @@
 # to make additional checks. This will allow checking stuff like nvme temps and
 # the RP1 southbridge temp.
 
+# To run this script you need to use sudo.
+# you can just put the file in your downloads folder
+# and run './Downloads/install-recommended.sh'
+# Note: set the script to allow anyone to execute before running it
+
 sudo apt install lm-sensors
 sudo apt install nvme-cli
+
+sudo apt install build-essential git meson
+git clone https://github.com/hotnuma/sysload.git
+cd sysload
+./install.sh
 
 # eof
